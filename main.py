c3a9e39b956e310b98108a7dcd59743a0a1901cd
Ahmad <ahmad007.me@gmail.com>
2022-05-15 10:52:01 +0100

    Added the git submodule that handles the peer-2-peer node connections and communications

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from tests.test import Tests as T
+from tests.test import Tests
 
 def main():
+    T = Tests()
     T.test_keys()
     T.test_timelock()
+    T.get_ip()
+    T.test_p2p()
 
 
 if __name__ == "__main__":
--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,21 @@
 import os
 import sys
 import datetime
-#import time
-sys.path.insert(1, os.getcwd())
+import time
+import socket  
+#sys.path.insert(1, os.getcwd())
+
+sys.path.insert(0, '..')  # Import the files where the modules are located
 
 from wallet.keys import Keys as k
 from blockchain.timelock import Timelock as timelock
+from app.p2p_network.p2pnetwork.node import Node
 
-class Tests:
+class Tests():
     def __init__(self) -> None:
         pass
 
-    def test_keys():
+    def test_keys(self):
         pr = k.gen_keys()
         f1 = 'pr_key.pem'
         k.save_key(pr, f1)
@@ -20,7 +24,7 @@ class Tests:
         print(_pr)
         print(_pu)
 
-    def test_timelock():
+    def test_timelock(self):
         #Use saved keys
         f1 = 'pr_key.pem'
         _pr = k.load_key(f1)
@@ -52,3 +56,57 @@ class Tests:
             print("Success! Good signature")
         else:
             print("Error!!! Bad Signature")
+
+    def test_p2p(self):
+        # Just for test we spin off multiple nodes, however it is more likely that these nodes are running
+        # on computers on the Internet! Otherwise we do not have any peer2peer application.
+        node_1 = Node("127.0.0.1", 8001, callback=self.node_callback)
+        node_2 = Node("127.0.0.1", 8002, callback=self.node_callback)
+        node_3 = Node("127.0.0.1", 8003, callback=self.node_callback)
+
+        time.sleep(1)
+        # node_1.debug = True
+        # node_2.debug = True
+        # node_3.debug = True
+        node_1.start()
+        node_2.start()
+        node_3.start()
+        time.sleep(1)
+
+        node_1.connect_with_node('127.0.0.1', 8002)
+        node_2.connect_with_node('127.0.0.1', 8003)
+        node_3.connect_with_node('127.0.0.1', 8001)
+
+        time.sleep(2)
+
+        node_1.send_to_nodes("message: hoi from node 1")
+
+        time.sleep(5)
+
+        node_1.stop()
+        node_2.stop()
+        node_3.stop()
+
+        print('end')
+
+    def node_callback(self, event, main_node, connected_node, data):
+        """The big callback method that gets all the events that happen inside the p2p network.
+
+        Implement here your own application logic. The event holds the event that occurred within
+        the network. The main_node contains the node that is handling the connection with and from
+        other nodes. An event is most probably triggered by the connected_node! If there is data
+        it is represented by the data variable."""
+        try:
+            # node_request_to_stop does not have any connected_node, while it is the main_node that is stopping!
+            if event != 'node_request_to_stop':
+                print('Event: {} from main node {}: connected node {}: {}'.format(event, main_node.id, connected_node.id,
+                                                                                data))
+
+        except Exception as e:
+            print(e)
+
+    def get_ip(self): 
+        hostname=socket.gethostname()   
+        IPAddr=socket.gethostbyname(hostname)   
+        print("Your Computer Name is:"+hostname)   
+        print("Your Computer IP Address is:"+IPAddr) 
